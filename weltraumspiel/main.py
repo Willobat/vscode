108f724e59d2f823ffaba11a4f740e83a47e6349
Willobat <willem@boeddecker.de>
2026-03-02 18:42:51 +0100

    ...

--- weltraumspiel/main.py
+++ weltraumspiel/main.py
@@ -1,9 +1,27 @@
-import math
+#weltraumbild Galaxie downscaled
 
-WIDTH = 900
-HEIGHT = 700
+
+import math
+import time
+import os
+
+WIDTH = 600
+HEIGHT = 800
+
+class Player:
+    def __init__ (self):
+        self.actor = Actor("spaceship")
+        self.actor.pos = (WIDTH // 2, HEIGHT // 1.1)
+    def show(self):
+        self.actor.draw()
+       
+player = Player()
+
+def draw():
+    player.show()
 
 def update():
     pass
-def draw():
-    screen.fill("black")
+
+
+
